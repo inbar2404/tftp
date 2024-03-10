f25f7766b2ebf0e39a9f9b3fb51ba7f0edf8aad0
BarShuv <barsh2001@gmail.com>
2024-03-10 21:56:06 +0200

    fixed receive port from client

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
@@ -4,8 +4,12 @@ import bgu.spl.net.srv.Server;
 
 public class TftpServer {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            args = new String[]{"7777"};
+        }
+
         Server.threadPerClient(
-                7777, //port
+                Integer.parseInt(args[0]), //port
                 () -> new TftpProtocol(), //protocol factory
                 TftpEncoderDecoder::new, //message encoder decoder factory
                 new ConnectionsImpl<>()
